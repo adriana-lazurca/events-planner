bc988e23b376253afa9789d391f31291dd0b63a6
Adriana Lazurca <adrianar.lazurca@gmail.com>
2023-01-20 16:35:55 +0100

    add new test

--- src/pages/EventsDashboard.test.tsx
+++ src/pages/EventsDashboard.test.tsx
@@ -1,6 +1,7 @@
+import { ReactNode } from 'react';
 import { render, screen } from '@testing-library/react';
 import { within } from '@testing-library/dom';
-import { ReactNode } from 'react';
+import userEvent from '@testing-library/user-event';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import MockAdapter from 'axios-mock-adapter';
 import axios from 'axios';
@@ -20,29 +21,61 @@ const ClientProvider = ({ children }: ClientProviderProps) => {
   return <QueryClientProvider client={queryClient}>{children} </QueryClientProvider>;
 };
 
-test('renders the events dashboard page', async () => {
-  render(
-    <ClientProvider>
-      <EventsDashboard />
-    </ClientProvider>,
-  );
+describe('Given: EventsDashboard component', () => {
+  describe('When rendering', () => {
+    test('Then it shows a table which is getting filled in with the correct data', async () => {
+      render(
+        <ClientProvider>
+          <EventsDashboard />
+        </ClientProvider>,
+      );
 
-  const titleColumn = await screen.findByText('Title');
-  expect(titleColumn).toBeInTheDocument();
+      const labelProperty = schema[0].label;
+      const titleColumn = await screen.findByText(labelProperty);
+      expect(titleColumn).toBeInTheDocument();
 
-  const headers = screen.getAllByRole('columnheader');
-  headers.forEach((header, index) => expect(header).toHaveTextContent(schema[index].label));
+      const headers = screen.getAllByRole('columnheader');
+      headers.forEach((header, index) => expect(header).toHaveTextContent(schema[index].label));
 
-  const rows = screen.getAllByRole('row');
-  const [, ...dataRows] = rows;
+      const rows = screen.getAllByRole('row');
+      const [, ...dataRows] = rows;
 
-  dataRows.forEach((row, rowIndex) => {
-    const event: any = events[rowIndex];
-    const cells = within(row).getAllByRole('cell');
+      dataRows.forEach((row, rowIndex) => {
+        const event: any = events[rowIndex];
+        const cells = within(row).getAllByRole('cell');
 
-    cells.forEach((cell, columnIndex) => {
-      const property = schema[columnIndex].name;
-      expect(cell).toHaveTextContent(event[property]);
+        cells.forEach((cell, columnIndex) => {
+          const nameProperty = schema[columnIndex].name;
+          expect(cell).toHaveTextContent(event[nameProperty]);
+        });
+      });
+    });
+  });
+
+  describe('When "Create event" button is clicked', () => {
+    test('it shows a form which is getting filled in with the correct structure', async () => {
+      render(
+        <ClientProvider>
+          <EventsDashboard />
+        </ClientProvider>,
+      );
+
+      const buttons = await screen.findAllByRole('button');
+      const createEventButton = buttons.find((button) => within(button).queryByText('Create event'));
+
+      expect(createEventButton).toBeInTheDocument();
+      createEventButton && userEvent.click(createEventButton);
+
+      const modal = await screen.findByRole('dialog');
+      expect(modal).toBeInTheDocument();
+
+      const formItems = modal.querySelectorAll('.ant-form-item');
+      expect(formItems).toHaveLength(schema.length);
+
+      schema.forEach((component) => {
+        const label = within(modal).getByTitle(component.label);
+        expect(label).toBeInTheDocument();
+      });
     });
   });
 });
